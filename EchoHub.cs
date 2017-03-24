f1d0d5cd8223ee65aebc1c3238567c29fe752bfa
solasolanin <asaokahide@yahoo.co.jp>
2017-03-25 01:03:43 +0900

    Add files via upload

---
+++ EchoHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Line2.Hubs
+{
+    [HubName("Echo")]
+    public class EchoHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+        public void Send(Data data)
+        {
+            Clients.All.Receive(data);
+        }
+    }
+
+    public class Data
+    {
+        public string User { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Line2.Startup))]
+
+namespace Line2
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // アプリケーションの設定方法の詳細については、http://go.microsoft.com/fwlink/?LinkID=316888 を参照してください
+            app.MapSignalR();
+        }
+    }
+}
